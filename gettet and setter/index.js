1d666a2b1994acf8dbc5060d1e859a2a297d97c4
Kristina <kira7999@gmail.com>
2021-08-12 14:34:03 +0300

    added gettters and setters 2 part

--- gettet and setter/index.js
+++ gettet and setter/index.js
@@ -1,33 +1,83 @@
 let dog = {
   legs: 4,
-  color: 'чёрный',
-  name: 'Bob',
+  color: "чёрный",
+  name: "Bob",
   teil: 1,
 
-  get dogProps(){
-    return `Это собака по имени ${this.name}. У неё ${this.teil} хвост, цввет: ${this.color}.`
-  }
-}
-console.log(
-  dog.dogProps
-);
+  get dogProps() {
+    return `Это собака по имени ${this.name}. У неё ${this.teil} хвост, цввет: ${this.color}.`;
+  },
+};
+console.log(dog.dogProps);
 
 let cat = {
   legs: 4,
-  color: 'рыжий',
-  name: 'Матроскин',
+  color: "рыжий",
+  name: "Матроскин",
   teil: 1,
 
-  get dogProps(){
-    return `Это кот по имени ${this.name}. У него ${this.teil} хвост, цввет: ${this.color}.`
+  get dogProps() {
+    return `Это кот по имени ${this.name}. У него ${this.teil} хвост, цввет: ${this.color}.`;
   },
 
   //позволим изменить объект
-  set catValueChange(value){
-    [this.name, this.color] = value.split(' ') 
-  }
-}
+  set catValueChange(value) {
+    [this.name, this.color] = value.split(" ");
+  },
+};
 
-cat.catValueChange = 'Марик белый'
+cat.catValueChange = "Марик белый";
 
 console.log(cat.dogProps);
+
+let human = {
+  name: "Bob",
+  surname: "Adams",
+};
+
+Object.defineProperty(human, "fullName", {
+  get() {
+    return `${this.name} ${this.surname}`;
+  },
+  set(value) {
+    [this.name, this.surname] = value.split(" ")
+  },
+});
+
+console.log(human.fullName);
+
+for(let k in human){
+  console.log(k);
+}
+
+let nameObj = {
+  get name() {
+    return this._name
+  },
+  set name(value){
+    if(value.length < 4){
+      console.log(`Имя ${value} меньще 4 символов`);
+      return
+    }
+    this._name = value
+  }
+}
+nameObj.name = 'Кирилл' // устанавливает в set name(value)
+console.log(nameObj._name); // получаем из get name()
+
+
+function User(name, birthday){
+  this.name = name;
+  this.birthday = birthday;
+
+  Object.defineProperty(this, 'age', {
+    get() {
+      let todayYear = new Date().getFullYear()
+      return todayYear - this.birthday.getFullYear()
+    }
+  })
+}
+
+let george = new User('Georg', new Date(1989, 7, 9))
+
+console.log(george.age);
