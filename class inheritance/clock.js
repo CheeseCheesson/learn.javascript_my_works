47ef3687a935a0e7433ef6a8004fd19ffa273727
Kristina <kira7999@gmail.com>
2021-08-26 09:31:08 +0300

    added clock

--- class inheritance/clock.js
+++ class inheritance/clock.js
@@ -1,48 +1,46 @@
 class Clock {
-    constructor({ template }) {
-      this.template = template;
-      console.log(template);
-    }
-  
-    render() {
-      let date = new Date();
-  
-      let hours = date.getHours();
-      if (hours < 10) hours = "0" + hours;
-  
-      let mins = date.getMinutes();
-      if (mins < 10) mins = "0" + mins;
-  
-      let secs = date.getSeconds();
-      if (secs < 10) secs = "0" + secs;
-  
-      let output = this.template
-        .replace("h", hours)
-        .replace("m", mins)
-        .replace("s", secs);
-  
-      console.log(output);
-    }
-  
-    stop() {
-      clearInterval(this.timer);
-    }
-  
-    start() {
-      this.render();
-      this.timer = setInterval(() => this.render(), 1000);
-    }
+  constructor({ template }, elem) {
+    this.template = template;
+    this.elem = elem
   }
+  render() {
+    let date = new Date();
 
-  class ExtendedClock extends Clock {
-    constructor(options) {
-      super(options);
-      let { precision=1000 } = options;
-      this.precision = precision;
+    let hours = date.getHours();
+    if (hours < 10) "0" + hours;
+
+    let mins = date.getMinutes();
+    if (mins < 10) "0" + mins;
+
+    let sec = date.getSeconds();
+    if (sec < 10) "0" + sec;
+
+    let output = this.template
+      .replace("h", hours)
+      .replace("m", mins)
+      .replace("s", sec);
+    console.log(output)
+  }
+  stop() {
+    clearInterval(this.timer);
+  }
+  start() {
+    this.render();
+    this.timer = setInterval(() => this.render(), 1000);
+  }
+}
+
+let clock = new Clock({ template: "h:m:s" });
+
+class ExtendetClock extends Clock{
+    constructor(options){
+        super(options)
+        let { precision = 1000} = options
+        this.precision = precision
     }
-  
-    start() {
-      this.render();
-      this.timer = setInterval(() => this.render(), this.precision);
+    start(){
+        this.render()
+        this.timer = setInterval(() => this.render(), this.precision)
     }
-  };
+}
+
