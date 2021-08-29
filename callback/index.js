0ec0684c02c5f96648ecd774742439b4d5e7a070
CheeseCheesson <kira7999@gmail.com>
2021-08-29 19:35:54 +0300

    add circle function, css, html

--- callback/index.js
+++ callback/index.js
@@ -1,8 +1,33 @@
-function loadScript(src) {
-  let script = document.createElement('script')
-  script.src = src
-  document.head.append(script)
-}
+let btn = document.querySelector('.btn')
 
-let falag = true
-loadScript('./test.js')
+let circle = function showCircle(cx, cy, radius){
+  let circle = document.createElement('div')
+  circle.style.width = 0
+  circle.style.height = 0
+  circle.style.left = cx + 'px'
+  circle.style.top = cy + 'px'
+  document.body.append(circle)
+  circle.classList.add('circleCSS')
+  circle.classList.add('hidden')
+  setTimeout(() => {
+    circle.textContent = 'Hello'
+  }, 3000)
+  setTimeout(()=>{
+    circle.style.width = radius * 2 + 'px';
+    circle.style.height = radius * 2 + 'px';
+  }, 2000)
+
+  return circle
+}
+let myShow = circle(150, 150, 150)
+
+btn.addEventListener('click', () => {
+  myShow.classList.toggle('hidden')
+  btn.classList.add('hidden')
+})
+
+
+// showCircle(150, 150, 100, div => {
+//   div.classList.add('message-ball');
+//   div.append("Hello, world!");
+// });
